40e9c95786252f6ff6a085a4a785fc95da96b529
liepinaievaa-maker <liepinaievaa@gmail.com>
2026-04-03 16:13:38 +0300

    Fix blog view route

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def my_blog(request):
+def blog_view(request):
     return HttpResponse("Hello, blog!")
--- codestar/urls.py
+++ codestar/urls.py
@@ -21,5 +21,4 @@ from blog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', views.blog_view),
-    path('admin/', admin.site.urls),
 ]
