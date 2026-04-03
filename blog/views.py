89aa4b4c5c8a623933ae5b48220b2d97454bc67f
liepinaievaa-maker <liepinaievaa@gmail.com>
2026-04-03 16:35:28 +0300

    Fix blog view and urls

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-def my_blog(request):
-    return HttpResponse("Hello, Blog!")
+def blog_view(request):
+    return HttpResponse("Hello, blog!")
