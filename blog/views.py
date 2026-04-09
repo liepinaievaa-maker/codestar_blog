6e99eecfad061a30310a341c76700385eb91a947
liepinaievaa-maker <liepinaievaa@gmail.com>
2026-04-09 15:38:59 +0300

    Add filtering

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,5 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(author=1)
+    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
