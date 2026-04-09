1c1419d2030f8f8332257e883adfce33fd7b8ce3
liepinaievaa-maker <liepinaievaa@gmail.com>
2026-04-09 14:01:35 +0300

    Add changes for blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-def blog_view(request):
-    return HttpResponse("Hello, blog!")
+# Create your views here.
+class PostList(generic.ListView):
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
