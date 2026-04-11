23184bc53b6b0f89e26ddb5a3c846659a20438ff
liepinaievaa-maker <liepinaievaa@gmail.com>
2026-04-11 11:50:10 +0300

    Fix static files for Heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -144,6 +144,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
