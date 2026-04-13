a0b26e5daed47f3ccacade3d968e6a7c59dfa6a4
liepinaievaa-maker <liepinaievaa@gmail.com>
2026-04-13 16:39:47 +0300

    Fix the static files

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1)
-    template_name = "blog/index.html"
-    paginate_by = 6
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
