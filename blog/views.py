cc4c865b88f6f1dc8b8808c6c0d882f1e847edbd
liepinaievaa-maker <liepinaievaa@gmail.com>
2026-04-14 00:53:28 +0300

    Pagionation for previous

--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,4 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog/index.html'
     context_object_name = 'post_list'
+    paginate_by = 6
